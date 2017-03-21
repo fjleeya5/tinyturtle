148312b058d3a0baa519a6f20a092923c0c394e1
fatema j leeya <fjleeya@gmail.com>
2017-03-21 20:13:44 +0000

    my comment

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 TinyTurtle.apply(window); 
+//function house(){
 /*global TinyTurtle*/
-forward(50);
+//forward(50);
 /*global forward*/
-right(90);
+//right(90);
 /*global right*/
-forward(50);
-right(90);
-forward(55);
-right (90);
-forward(50);
-right(90);
-forward(55);
-right (40);
-forward(30);
-right(87);
-forward(25);
-stamp();
-/*global stamp*/
+//forward(50);
+//right(90);
+//forward(55);
+//right (90);
+//forward(50);
+//right(90);
+//forward(55);
+//right (40);
+//forward(30);
+//right(87);
+//forward(25);
+//stamp();
+function triangle(){
+ right(35);
+ forward(80);
+ right(100);
+forward(80);
+    
+}
+triangle();
+stamp()
